ff3cc90490acfa4638ec1478d29d6d4019eda337
Dae Platform <36157082+daeplatform@users.noreply.github.com>
2018-07-03 04:49:11 +0700

    Daethereum By Dae Platform

---
+++ daethereum.sol
@@ -0,0 +1,16 @@
+**
+* pragma solidity ^0.4.23
+* Contract Name: Daethereum
+* Token Name: Daethereum
+* Token Symbol: DTHR
+* Compiler Version: v0.4.24+commit.e67f0147
+* Optimization Enabled: No
+* Runs (Optimiser): 200
+**
+
+import './ERC20Basic.sol';
+import './SafeMath.sol';
+import './Ownable.sol';
+import './BasicToken.sol';
+import './StandardToken.sol';
+import './Daethereum.sol';
