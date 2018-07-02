673072fbdbfba01ff250541302adff9371ee9e0a
Dae Platform <36157082+daeplatform@users.noreply.github.com>
2018-07-03 04:33:11 +0700

    contract ERC20 is ERC20Basic

--- ERC20.sol
+++ ERC20.sol
@@ -6,7 +6,7 @@ pragma solidity ^0.4.23;
  * @title ERC20 interface
  * @dev see https://github.com/ethereum/EIPs/issues/20
  */
-contract ERC20ERC20 is ERC20Basic {
+contract ERC20 is ERC20Basic {
   function allowance(address owner, address spender)
     public view returns (uint256);
 
